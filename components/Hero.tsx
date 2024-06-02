c9db44b0a560f75f660cfe61270363c705ec4e0f
YoussefMassoud <youssefbusiness133@gmail.com>
2024-06-02 16:48:09 +0300

    make an edit in text

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,15 +2,11 @@ import { FaLocationArrow } from "react-icons/fa6";
 
 import MagicButton from "./MagicButton";
 import { Spotlight } from "./ui/Spotlight";
-import { TextGenerateEffect } from "./ui/TextGenerateEffect";
+import { TypewriterEffectSmooth } from "./ui/typewriter-effect";
 
 const Hero = () => {
   return (
     <div className="pb-20 pt-36">
-      {/**
-       *  UI: Spotlights
-       *  Link: https://ui.aceternity.com/components/spotlight
-       */}
       <div>
         <Spotlight
           className="-top-40 -left-10 md:-left-32 md:-top-20 h-screen"
@@ -23,18 +19,11 @@ const Hero = () => {
         <Spotlight className="left-80 top-28 h-[80vh] w-[50vw]" fill="blue" />
       </div>
 
-      {/**
-       *  UI: grid
-       *  change bg color to bg-black-100 and reduce grid color from
-       *  0.2 to 0.03
-       */}
       <div
         className="h-screen w-full dark:bg-black-100 bg-white dark:bg-grid-white/[0.03] bg-grid-black-100/[0.2]
        absolute top-0 left-0 flex items-center justify-center"
       >
-        {/* Radial gradient for the container to give a faded look */}
         <div
-          // chnage the bg to bg-black-100, so it matches the bg color and will blend in
           className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100
          bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"
         />
@@ -46,13 +35,15 @@ const Hero = () => {
             Dynamic Web Magic with Next.js
           </p>
 
-          {/**
-           *  Link: https://ui.aceternity.com/components/text-generate-effect
-           *
-           *  change md:text-6xl, add more responsive code
-           */}
-          <TextGenerateEffect
-            words="Transforming Concepts into Seamless User Experiences"
+          <TypewriterEffectSmooth
+            words={[
+              { text: "Transforming" },
+              { text: "Concepts" },
+              { text: "into" },
+              { text: "Seamless" },
+              { text: "User" },
+              { text: "Experiences" },
+            ]}
             className="text-center text-[40px] md:text-5xl lg:text-6xl"
           />
 
