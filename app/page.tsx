e18ca03b9ff4d50071999c3527e612289f5a77a2
YoussefMassoud <youssefbusiness133@gmail.com>
2024-06-02 19:57:20 +0300

    remove file donot need it

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,6 @@
 import { navItems } from "@/data";
 
 import Hero from "@/components/Hero";
-import Grid from "@/components/Grid";
 import Footer from "@/components/Footer";
 import Clients from "@/components/Clients";
 import Approach from "@/components/Approach";
@@ -16,7 +15,6 @@ const Home = () => {
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
         <Hero />
-        <Grid />
         <RecentProjects />
         <Clients />
         <Experience />
