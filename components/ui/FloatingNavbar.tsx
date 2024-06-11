d01ede6230d91fa88588b337a5e65da7eb91b7de
YoussefMassoud <youssefbusiness133@gmail.com>
2024-06-11 04:22:05 +0300

    back to defualt nav thank you

--- components/ui/FloatingNavbar.tsx
+++ components/ui/FloatingNavbar.tsx
@@ -6,8 +6,8 @@ import {
   useScroll,
   useMotionValueEvent,
 } from "framer-motion";
-import { cn } from "@/utils/cn";
 import Link from "next/link";
+import { cn } from "@/lib/utils";
 import { usePathname } from "next/navigation";
 
 export const FloatingNav = ({
@@ -24,15 +24,13 @@ export const FloatingNav = ({
   const { scrollYProgress } = useScroll();
 
   const [visible, setVisible] = useState(true);
-  const pathname = usePathname();
 
   useMotionValueEvent(scrollYProgress, "change", (current) => {
-    // Check if current is not undefined and is a number
     if (typeof current === "number") {
       let direction = current! - scrollYProgress.getPrevious()!;
 
       if (scrollYProgress.get() < 0.05) {
-        setVisible(false);
+        setVisible(true);
       } else {
         if (direction < 0) {
           setVisible(true);
@@ -42,6 +40,7 @@ export const FloatingNav = ({
       }
     }
   });
+  const pathname = usePathname();
 
   return (
     <AnimatePresence mode="wait">
@@ -51,32 +50,38 @@ export const FloatingNav = ({
           y: -100,
         }}
         animate={{
-          y: visible ? 0 : -100,
-          opacity: visible ? 1 : 0,
+          y: 0,
+          opacity: 1,
         }}
         transition={{
           duration: 0.2,
         }}
         className={cn(
-          "flex max-w-fit  fixed top-10 inset-x-0 mx-auto border border-transparent dark:border-white/[0.2] rounded-xl dark:bg-[#0d1429] bg-white shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] z-[5000] pr-2 p-8 py-6  items-center justify-center space-x-4",
+          "flex max-w-fit md:min-w-[70vw] lg:min-w-fit fixed z-[5000] top-10 inset-x-0 mx-auto px-10 py-5 rounded-lg border border-black/.1 shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] items-center justify-center space-x-4",
           className
         )}
+        style={{
+          backdropFilter: "blur(16px) saturate(180%)",
+          backgroundColor: "rgba(17, 25, 40, 0.75)",
+          borderRadius: "12px",
+          border: "1px solid rgba(255, 255, 255, 0.125)",
+        }}
       >
-        {navItems.map((navItem: any, idx: number) => (
-               <Link
-               key={`link=${idx}`}
-               href={navItem.link}
-               className={cn(
-                 "relative text-neutral-50 items-center flex space-x-1  hover:text-neutral-300 "
-               )}
-             >
-               <span className={
-                 pathname === navItem.link
-                   ? " text-blue-600"
-                   : ""
-               }>{navItem.name}</span>
-             </Link>
-        ))}
+          {navItems.map((navItem: any, idx: number) => (
+              <Link
+                key={`link=${idx}`}
+                href={navItem.link}
+                className={cn(
+                  "relative text-neutral-50 items-center flex space-x-1  hover:text-neutral-300 "
+                )}
+              >
+                <span className={
+                  pathname === navItem.link
+                    ? " text-blue-600"
+                    : ""
+                }>{navItem.name}</span>
+              </Link>
+          ))}
       </motion.div>
     </AnimatePresence>
   );
