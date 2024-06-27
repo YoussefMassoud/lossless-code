d4a5a4c1010b54712318b772dd563dbf0ec8c16d
YoussefMassoud <youssefbusiness133@gmail.com>
2024-06-28 02:14:23 +0300

    REMOVE IMAGE DONOT NEED IT

--- data/index.ts
+++ data/index.ts
@@ -3,6 +3,7 @@ export const navItems = [
   { name: "About", link: "/about" },
   { name: "Projects", link: "/projects" },
   { name: "Contact", link: "#contact" },
+
 ];
 
 export const projects = [
@@ -77,17 +78,3 @@ export const workExperience = [
   },
 ];
 
-export const socialMedia = [
-  {
-    id: 1,
-    img: "/git.svg",
-  },
-  {
-    id: 2,
-    img: "/twit.svg",
-  },
-  {
-    id: 3,
-    img: "/link.svg",
-  },
-];
